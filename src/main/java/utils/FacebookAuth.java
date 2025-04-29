d6c956f64bc76ef38247a4734294eb728dcc41db
mrsanji <azerfathalli6@gmail.com>
2025-04-29 20:16:53 +0100

    Facebook Sign Up

--- src/main/java/utils/FacebookAuth.java
+++ src/main/java/utils/FacebookAuth.java
@@ -1,4 +1,21 @@
 package utils;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.util.UUID;
+
 public class FacebookAuth {
+    private static final String APP_ID = "";
+    private static final String REDIRECT_URI = "http://localhost:8888/fb-callback";
+
+    public static void launchLogin() throws IOException {
+        String url = "https://www.facebook.com/v19.0/dialog/oauth?" +
+                "client_id=" + APP_ID +
+                "&redirect_uri=" + URLEncoder.encode(REDIRECT_URI, "UTF-8") +
+                "&state=" + UUID.randomUUID().toString() +
+                "&scope=email,public_profile";
+        Desktop.getDesktop().browse(URI.create(url));
+    }
 }
